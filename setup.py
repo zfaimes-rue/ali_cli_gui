e0aa8cad02638ef666281aef9e72ce65861ed7bf
zfaimes-rue <zfaimes-rue@users.noreply.github.com>
2024-12-14 11:01:03 +0000

    Update: Adds offline data synchronization

--- setup.py
+++ setup.py
@@ -1,1 +1,3 @@
 from setuptools import setup, find_packages\n\nsetup(\n    name='ali_cli_gui',\n    version='1.0.0',\n    packages=find_packages(),\n    install_requires=[]\n)
+
+# PR Update: 2025-10-21 - docs/update-8311
